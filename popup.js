2ad32230bda1aa9fe4feef8f8ca1255955b3d9df
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-05-18 17:40:23 -0400

    Delete popup.js

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("toggleFilter").addEventListener("click", () => {
-    chrome.scripting.executeScript({
-        target: { tabId: chrome.tabs.TAB_ID_NONE },
-        func: filterComments
-    });
-});
