adc58ae7a49b342b8ec6021e6a45c285e662deda
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-10-29 10:04:45 -0400

    Add files via upload

---
+++ popupContent.js
@@ -0,0 +1,15 @@
+document.getElementById("NameofDev").innerText = "Jahim Sankoh";
+
+document.getElementById("ver").innerText = "Release " + chrome.runtime.getManifest().version;
+
+function goToPage() {
+    window.location.href = "https://github.com/Loading617/ComFil";
+}
+
+function gotoPage() {
+    window.location.href = "https://comfil.com/changelog";
+}
+
+function gotoPage() {
+    window.location.href = "https://github.com/Loading617/ComFil/issues";
+}
